77f118a3bf947bcf16ff3fa1d94336e48c012231
YulinarNR <71864701+YulinarNR@users.noreply.github.com>
2020-10-16 23:32:42 +0700

    Tugas WSIBW (15-10-2020)

---
+++ E41191151_Yulinar Nur Rahmawati/Tugas1/tipe_data.php
@@ -0,0 +1,22 @@
+<?php
+//variabel merupakan tipe data string yang berisi text
+$tes = "Saya Pergi Kerja";
+
+//tipe data integer
+$bilangan_pertama = 12;
+$bilangan_kedua = 78;
+
+
+//tipe data float berisi bilangan desimal
+$angka = 12.177;
+
+
+//tipe data boolean dengan 2 value yaitu true & false
+$x = false;
+$y = true;
+
+//tipe data array yg memiliki banyak data dalam satu variabel
+$anggota = array("Andi","Budi","Joni");
+
+
+?>
