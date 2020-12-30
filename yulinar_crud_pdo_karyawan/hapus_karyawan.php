0a03656a07fe51449da63c74dea927eca11bc05f
YulinarNR <yulinarnurrahma@gmail.com>
2020-12-30 23:28:54 +0700

    Removed folder from repository

--- yulinar_crud_pdo_karyawan/hapus_karyawan.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-$query = "DELETE FROM karyawan WHERE id_karyawan='$_GET[id]'";
-$data = $koneksi->prepare($query);
-$data->execute();
-
-echo "<script type='text/javascript'>location.href=\"index.php\"; </script>";
-?>
--- yulinar_crud_pdo_karyawan/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-$koneksi = new PDO('mysql:host=localhost;dbname=db_pdo', 'root', '');
-
-?>
