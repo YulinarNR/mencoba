da25f35b2bdd8d75a5bc394977ecd67a1897babb
YulinarNR <yulinarnurrahma@gmail.com>
2021-01-02 10:52:58 +0700

    koneksi

---
+++ ebusi2/koneksi/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+//membuat koneksi dengan database
+$koneksi = mysqli_connect("localhost","root","","db_kebun27");
+// if ($koneksi) {
+// 	echo "Koneksi Sukses";
+// } else {
+// 	echo "Koneksi Gagal";
+// }
+?>
